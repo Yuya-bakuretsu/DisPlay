2865a1f33a3257707545e5e3a2c9a20a42ece9e8
Yuya-bakuretsu <suzaku.3077kandai@gmail.com>
2020-08-19 18:15:05 +0900

    add custom_add views and custom model

--- task/models.py
+++ task/models.py
@@ -7,8 +7,8 @@ import datetime
 class Custom(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    # start_time = models.TimeField('開始時間', dafault=datetime.time(0, 0, 0))
-    # end_time = models.TimeField('終了時間', dafault=datetime.time(0, 0, 0))
+    start_time = models.TimeField('開始時間', default=datetime.time(0, 0, 0))
+    end_time = models.TimeField('終了時間', default=datetime.time(0, 0, 0))
 
     def __str__(self):
         return self.title
--- task/urls.py
+++ task/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     # path('', views.top_menu, name='top_menu'),
     # path('', views.initial_settings, name='initial_settings'),
-    path('', views.custom_list(), name='custom_list'),
+    path('', views.custom_list, name='custom_list'),
+    # path('add_custom/', views.custom_add, name='custom_add'),
 ]
