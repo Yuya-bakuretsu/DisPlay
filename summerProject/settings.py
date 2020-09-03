12fccbd98d286a569205eeeb07fc41a438c6865a
Yuya-bakuretsu <suzaku.3077kandai@gmail.com>
2020-09-03 11:30:09 +0900

    make form and now we can add todo

--- summerProject/settings.py
+++ summerProject/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'task.apps.TaskConfig'
+    'task.apps.TaskConfig',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
