812bdd6a158a75fa3379de70125879b90a89cb7b
Yuya-bakuretsu <suzaku.3077kandai@gmail.com>
2020-09-23 17:51:38 +0900

    add table for todo_list.html

--- task/views.py
+++ task/views.py
@@ -81,6 +81,10 @@ class TodoListView(ListView):
     model = Todo
     paginate_by = 10
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["customs"] = Custom.objects.all()
+        return context
 
 class TodoAddView(CreateView):
     model = Todo
