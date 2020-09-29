7d9b11140fb5230725296acd477ad25ed251c627
Yuya-bakuretsu <suzaku.3077kandai@gmail.com>
2020-09-29 18:27:17 +0900

    create authentication

--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import VueSession from 'vue-session'
+
+Vue.use(Vuetify)
+Vue.use(VueSession)
 
 Vue.config.productionTip = false
 
