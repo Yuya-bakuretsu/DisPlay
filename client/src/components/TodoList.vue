8540df10547774109f787693a4d11e9cae4e98ec
Kissa0330 <raika4789@gmail.com>
2020-10-14 17:49:05 +0900

    create TodoList file

---
+++ client/src/components/TodoList.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <p>test</p>
+  </div>
+</template>
+<style src='../static/TodoList.css'></style>
+<script>
+export default {
+  name: 'TodoList',
+}
+</script>
--- client/src/views/Toppage.vue
+++ client/src/views/Toppage.vue
@@ -2,18 +2,21 @@
   <div>
     <Header></Header>
     <Chart></Chart>
+    <TodoList></TodoList>
   </div>
 </template>
 <style></style>
 <script>
 import Header from "../components/Header";
 import Chart from "../components/Chart";
+import TodoList from "../components/TodoList";
 
 export default {
   name: "toppage",
   components: {
     Header,
     Chart,
+    TodoList,
   },
 };
 </script>
