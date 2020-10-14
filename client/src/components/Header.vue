614dd4521df7502ce1b0774fd2469d5cf9c82651
Kissa0330 <raika4789@gmail.com>
2020-10-14 18:12:41 +0900

    change style.css

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="Header">
       <!-- TODO:Headertitleを今のページによって変える -->
-      <h2 class="Headertitle">DisPlay</h2>
+      <h1 class="Headertitle">DisPlay</h1>
       <img src="../assets/Gear.svg" alt="gear" class="Gear" />
     </div>
   </div>
--- client/src/components/TodoList.vue
+++ client/src/components/TodoList.vue
@@ -1,11 +1,31 @@
 <template>
   <div>
-    <p>test</p>
+    <div class="Todo_menu">
+      <h3>Todolist</h3>
+      <button class="Landscape_button">
+        <div class="button_text">Add</div></button
+      ><img
+        class="Trashcan"
+        src="../assets/Trashcan.svg"
+        alt="trashcan"
+      />
+    </div>
+    <div class="Task_list">
+      <div class="Task">
+        <h4>郵便局に荷物出す</h4>
+        <button class="Circle_button Play">
+          <img class="Play_img" src="../assets/Play.svg" alt="Play" /></button
+        ><button class="Circle_button Check">
+          <img src="../assets/Check.svg" alt="check" />
+        </button>
+        <p>Deadline 8/20</p>
+      </div>
+    </div>
   </div>
 </template>
-<style src='../static/TodoList.css'></style>
+<style src="../static/TodoList.css"></style>
 <script>
 export default {
-  name: 'TodoList',
-}
+  name: "TodoList",
+};
 </script>
