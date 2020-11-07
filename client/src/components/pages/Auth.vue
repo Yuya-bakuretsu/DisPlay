9ac5e04e1d658b9f6fd73df8a70666cff7a269ba
Yuya-bakuretsu <suzaku.3077kandai@gmail.com>
2020-11-07 21:18:42 +0900

    solve conflict come before develop with auth.vue

--- client/src/components/pages/Auth.vue
+++ client/src/components/pages/Auth.vue
@@ -95,7 +95,7 @@ export default {
             }).catch(e => {
                 this.loading = false;
                 Swal.fire({
-                icon: 'warning',
+                type: 'warning',
                 title: 'Error',
                 text: 'ユーザー名もしくはパスワード、または両方が間違っています',
                 showConfirmButton:false,
